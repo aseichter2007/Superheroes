0c42f378da6725943053742d02938350810f752c
aseichter2007 <aseichter2007@gmail.com>
2020-05-19 11:54:07 -0500

    Finished superherocontroller.cs. tested all funcions working as intended.

--- Superheroes/Controllers/HomeController.cs
+++ Superheroes/Controllers/HomeController.cs
@@ -5,22 +5,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Superheroes.Data;
 using Superheroes.Models;
 
 namespace Superheroes.Controllers
 {
     public class HomeController : Controller
     {
+        ApplicationDbContext _Context;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext _Context)
         {
+            this._Context = _Context;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
-            return View();
+            SuperHero hero = _Context.heroes.Where(h => h.favorite == true).Single();
+            return View(hero);
         }
 
         public IActionResult Privacy()
--- Superheroes/Controllers/SuperHeroController.cs
+++ Superheroes/Controllers/SuperHeroController.cs
@@ -26,24 +26,32 @@ namespace Superheroes.Controllers
         // GET: SuperHero/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            SuperHero hero = _Context.heroes.Where(h => h.Id == id).Single();
+            return View(hero);
         }
 
         // GET: SuperHero/Create
         public ActionResult Create()
         {
-            return View();
+            SuperHero hero = new SuperHero();
+            return View(hero);
         }
 
         // POST: SuperHero/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(SuperHero hero)
         {
+            
             try
             {
-                // TODO: Add insert logic here
-
+                var notfavorite = _Context.heroes.Where(h => h.favorite == true);
+                foreach (var item in notfavorite)
+                {
+                    item.favorite = false;
+                }
+                _Context.heroes.Add(hero);
+                _Context.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -55,18 +63,28 @@ namespace Superheroes.Controllers
         // GET: SuperHero/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            SuperHero hero = _Context.heroes.Where(h => h.Id == id).Single();
+
+            return View(hero);
         }
 
         // POST: SuperHero/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, SuperHero hero)
         {
             try
             {
-                // TODO: Add update logic here
-
+                if (hero.favorite==true)
+                {
+                    var notfavorite = _Context.heroes.Where(h => h.favorite == true);
+                    foreach (var item in notfavorite)
+                    {
+                        item.favorite = false;
+                    }
+                }
+                _Context.heroes.Update(hero);
+                _Context.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -78,17 +96,21 @@ namespace Superheroes.Controllers
         // GET: SuperHero/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            SuperHero hero = _Context.heroes.Where(h => h.Id == id).Single();
+
+            return View(hero);
         }
 
         // POST: SuperHero/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(int id, SuperHero hero)
         {
             try
             {
-                // TODO: Add delete logic here
+                 //SuperHero hero = _Context.heroes.Find(id);
+                 _Context.heroes.Remove(hero);
+                 _Context.SaveChanges();
 
                 return RedirectToAction(nameof(Index));
             }
